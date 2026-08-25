70dd1d5db0e606c4294f5cda6da2b9bb7ba5709e
Matteo Marrucci <0407matteo.marrucci@gmail.com>
2026-08-25 12:08:09 +0200

    added hyprland.lua file

---
+++ hyprland.lua
@@ -0,0 +1,209 @@
+-- NOTE: some settings are better configured in a 
+-- separate file, since they contain hardware- and
+-- device-specific configuration. Following is an 
+-- example of a single monitor configuration:
+--
+-- hl.monitor({
+--     output   = "",
+--     mode     = "2560x1440@240",
+--     position = "auto",
+--     scale    = "1",
+-- })
+--
+-- The following line takes the specific configuration
+-- from a file named "specific.lua" placed inside
+-- of this folder. Change this line to change the path
+-- of the file.
+require("specific")
+
+-- DEFAULTS
+local terminal    = "kitty"
+local fileManager = "dolphin"
+local menu        = "rofi -show run"
+
+-- AUTORUN
+hl.on("hyprland.start", function()
+    hl.exec_cmd("systemctl --user start hyprpolkitagent")
+    hl.exec_cmd("waybar")
+    hl.exec_cmd("hyprpaper") -- hyprpaper can be configured with a "hyprpaper.conf" file placed inside of this folder
+    hl.exec_cmd("cliphist")
+    hl.exec_cmd("dunst")
+end)
+
+-- ENVIRONMENT VARIABLES
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+
+-- PERMISSIONS
+hl.config({
+  ecosystem = {
+    enforce_permissions = true,
+  },
+})
+
+hl.permission({binary = "/usr/bin/grim", type = "screencopy", mode = "allow"})
+-- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
+-- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
+
+-- GENERAL CONFIG
+hl.config({
+	input = {
+		kb_layout = "us",
+		follow_mouse = 1,
+		sensitivity = 0,
+		touchpad = {
+			natural_scroll = true,
+		},
+	},
+
+	general = {
+		gaps_in = 0,
+		gaps_out = 0,
+		border_size = 0,
+		col = {
+			active_border = "rgba(40, 85, 119, 1)",
+			inactive_border = "rgba(595959aa)",
+		},
+		resize_on_border = false,
+		allow_tearing = false,
+		layout = "dwindle",
+	},
+
+	decoration = {
+		rounding = 0,
+		rounding_power = 2,
+		active_opacity = 1.0,
+		inactive_opacity = 1.0,
+		blur = {
+			enabled = false,
+		},
+		shadow = {
+			enabled = true,
+			range = 4,
+			render_power = 3,
+			color = "rgba(1a1a1aee)",
+		},
+	},
+
+	animations = {
+		enabled = false,
+	},
+
+    dwindle = {
+        preserve_split = true,
+    },
+
+    misc = {
+        disable_hyprland_logo = true,
+        force_default_wallpaper = 0,
+    },
+
+    ecosystem = {
+        no_update_news = true,
+        no_donation_nag = true,
+    },
+
+    xwayland = {
+        force_zero_scaling = true
+    }
+})
+
+-- BINDS
+local mainMod = "SUPER"
+
+hl.bind(mainMod .. " + backslash", hl.dsp.exec_cmd("grim"))
+hl.bind(mainMod .. " + SHIFT + backslash", hl.dsp.exec_cmd("grim -g \"$(slurp -d)\""))
+hl.bind(mainMod .. " + CTRL + backslash", hl.dsp.exec_cmd("grim - | wl-copy"))
+hl.bind(mainMod .. " + CTRL + SHIFT + backslash", hl.dsp.exec_cmd("grim -g \"$(slurp -d)\" - | wl-copy"))
+
+hl.bind(mainMod .. " + SHIFT + F", hl.dsp.exec_cmd("firefox"))
+hl.bind(mainMod .. " + SHIFT + S", hl.dsp.exec_cmd("steam"))
+hl.bind(mainMod .. " + SHIFT + E", hl.dsp.exec_cmd("emacs"))
+hl.bind(mainMod .. " + SHIFT + Z", hl.dsp.exec_cmd("zathura"))
+hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
+
+hl.bind(mainMod .. " + C", hl.dsp.window.close())
+hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen())
+
+hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
+hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
+hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
+
+hl.bind(mainMod .. " + H",  hl.dsp.focus({ direction = "left" }))
+hl.bind(mainMod .. " + L", hl.dsp.focus({ direction = "right" }))
+hl.bind(mainMod .. " + K",    hl.dsp.focus({ direction = "up" }))
+hl.bind(mainMod .. " + J",  hl.dsp.focus({ direction = "down" }))
+
+hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left" }))
+hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
+hl.bind(mainMod .. " + up",    hl.dsp.focus({ direction = "up" }))
+hl.bind(mainMod .. " + down",  hl.dsp.focus({ direction = "down" }))
+
+for i = 1, 10 do
+    local key = i % 10 -- 10 maps to key 0
+    hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
+    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+end
+
+hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
+hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
+
+hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
+
+hl.bind(mainMod .. " + SHIFT + H", hl.dsp.window.swap({direction = "left"}))
+hl.bind(mainMod .. " + SHIFT + L", hl.dsp.window.swap({direction = "right"}))
+hl.bind(mainMod .. " + SHIFT + K", hl.dsp.window.swap({direction = "up"}))
+hl.bind(mainMod .. " + SHIFT + J", hl.dsp.window.swap({direction = "down"}))
+
+hl.bind(mainMod .. " + SHIFT + left", hl.dsp.window.swap({direction = "left"}))
+hl.bind(mainMod .. " + SHIFT + right", hl.dsp.window.swap({direction = "right"}))
+hl.bind(mainMod .. " + SHIFT + up", hl.dsp.window.swap({direction = "up"}))
+hl.bind(mainMod .. " + SHIFT + down", hl.dsp.window.swap({direction = "down"}))
+
+-- Laptop Multimedia Keys
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
+hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
+hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+
+-- Requires playerctl
+hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+
+-- WINDOW RULES (DEFAULT EXAMPLES)
+hl.window_rule({
+    -- Ignore maximize requests from all apps.
+    name  = "suppress-maximize-events",
+    match = { class = ".*" },
+    suppress_event = "maximize",
+})
+
+hl.window_rule({
+    -- Fix some dragging issues with XWayland
+    name  = "fix-xwayland-drags",
+    match = {
+        class      = "^$",
+        title      = "^$",
+        xwayland   = true,
+        float      = true,
+        fullscreen = false,
+        pin        = false,
+    },
+
+    no_focus = true,
+})
+
+-- Hyprland-run windowrule
+hl.window_rule({
+    name  = "move-hyprland-run",
+    match = { class = "hyprland-run" },
+
+    move  = "20 monitor_h-120",
+    float = true,
+})
